45c593a8cf12729829f70f1cf4fdc8c44afebfc9
Anton Tsyrkunov <antontsyrkunov@gmail.com>
2023-01-17 14:44:46 +0200

    header & modal reprocessed

--- src/js/modal.js
+++ src/js/modal.js
@@ -28,8 +28,42 @@
     }
   })();
 
+  // const refs = {
+  //   openModalBtn: document.querySelector('[data-modal-open]'),
+  //   closeModalBtns: document.querySelectorAll('[data-modal-close]'),
+  //   sendModalBtn: document.querySelector('[data-action="send"'),
+  //   modal: document.querySelector('[data-modal]'),
+  //   modalForm: document.querySelector('.modal-wrapper'),
+  //   gratitudePopup: document.querySelector('.modal-gratitude'),
+  //   spinnerEl: document.querySelector('.spinner'),
+  // };
+  
+  // refs.openModalBtn.addEventListener('click', toggleModal);
+  // refs.closeModalBtns.forEach(closeBtn => {
+  //   closeBtn.addEventListener('click', toggleModal);
+  // });
+  // refs.sendModalBtn.addEventListener('click', toggleModal);
+  
+  // function toggleModal(event) {
+  //   event.preventDefault();
+  //   if (event.target.dataset.action === 'send') {
+  //     refs.spinnerEl.classList.remove('visually-hidden');
+  //     refs.modalForm.classList.add('visually-hidden');
+  //     setTimeout(() => {
+  //       refs.gratitudePopup.classList.remove('visually-hidden');
+  //       refs.spinnerEl.classList.add('visually-hidden');
+  //     }, 3000);
+  //   } else {
+  //     refs.modal.classList.toggle('is-hidden');
+  //     setTimeout(() => {
+  //       refs.modalForm.classList.remove('visually-hidden');
+  //       refs.gratitudePopup.classList.add('visually-hidden');
+  //     }, 3000);
+  //   }
+  // }
+
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
+    openModalBtns: document.querySelectorAll('[data-modal-open]'),
     closeModalBtns: document.querySelectorAll('[data-modal-close]'),
     sendModalBtn: document.querySelector('[data-action="send"'),
     modal: document.querySelector('[data-modal]'),
@@ -38,7 +72,10 @@
     spinnerEl: document.querySelector('.spinner'),
   };
   
-  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtns.forEach(openBtn => {
+    openBtn.addEventListener('click', toggleModal);
+  }); // add function to select all buttons that open modal window
+  
   refs.closeModalBtns.forEach(closeBtn => {
     closeBtn.addEventListener('click', toggleModal);
   });
@@ -52,12 +89,12 @@
       setTimeout(() => {
         refs.gratitudePopup.classList.remove('visually-hidden');
         refs.spinnerEl.classList.add('visually-hidden');
-      }, 3000);
+      }, 3000); // set time to toogle popup view
     } else {
       refs.modal.classList.toggle('is-hidden');
       setTimeout(() => {
         refs.modalForm.classList.remove('visually-hidden');
         refs.gratitudePopup.classList.add('visually-hidden');
-      }, 3000);
+      }, 500);
     }
   }
